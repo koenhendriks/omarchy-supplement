3d6c92b2f830ed0304f90d775e47d0ea390dc3ef
Koen Hendriks <info@koenhendriks.nl>
2026-08-10 11:20:07 +0200

    Register all new install scripts in install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # Install all packages in order
-. ./install-glab
+. ./install-browser.sh
+. ./remove-default-browsers.sh
+. ./remove-webapps.sh
+. ./install-glab.sh
+. ./install-teams.sh
+. ./install-outlook.sh
+. ./install-yaak.sh
+. ./install-le-ca.sh
+. ./install-openvpn.sh
+. ./install-strongswan.sh
+. ./install-vpn.sh
 . ./install-hyprland-overrides.sh
+. ./install-waybar-config.sh
