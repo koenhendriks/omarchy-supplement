fb64edbbd74de9c9687b85f70fddebc4898f2e4d
Koen Hendriks <info@koenhendriks.nl>
2026-08-10 11:19:43 +0200

    Reset webapps and add Teams and Outlook installs

---
+++ install-outlook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+omarchy webapp install Outlook https://outlook.office.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/microsoft-outlook.png
---
+++ remove-webapps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+omarchy webapp remove all
