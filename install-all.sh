16a05851bae43bc9c16dbcc5c9f5b16d80fed602
Koen Hendriks <info@koenhendriks.nl>
2026-08-24 15:36:52 +0200

    add xwaylad video bridge

--- install-all.sh
+++ install-all.sh
@@ -32,6 +32,7 @@ fi
 # Install all packages in order
 . ./copy-resources.sh
 . ./install-snap-pac.sh
+. ./install-xwaylandvideobridge.sh
 . ./install-browser.sh
 . ./remove-default-browsers.sh
 . ./remove-webapps.sh
